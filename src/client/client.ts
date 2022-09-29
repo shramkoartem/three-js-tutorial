8300ff2289e4d977c43fcb709ec6f6610bdf0ea4
shramkoartem <art.shramko@gmail.com>
2022-09-29 23:51:10 +0200

    shadow

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,82 +1,177 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { DragControls } from 'three/examples/jsm/controls/DragControls';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
-// scene, camera, renderer
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-const scene = new THREE.Scene();
-scene.add( new THREE.AxesHelper(5) );
+const light = new THREE.SpotLight()
+light.castShadow = true;
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 100
+scene.add(light)
+
+// const helper = new THREE.SpotLightHelper(light)
+const helper = new THREE.CameraHelper(light.shadow.camera);
+scene.add(helper)
+
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
-);
-camera.position.z = 5;
-camera.position.y = 1;
+)
+camera.position.z = 7
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+//renderer.shadowMap.type = THREE.BasicShadowMap
+//renderer.shadowMap.type = THREE.PCFShadowMap
+//renderer.shadowMap.type = THREE.VSMShadowMap
+document.body.appendChild(renderer.domElement)
 
-const render = () => {
-    renderer.render(scene, camera);
+new OrbitControls(camera, renderer.domElement)
+
+const planeGeometry = new THREE.PlaneGeometry(100, 20)
+const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
+plane.rotateX(-Math.PI / 2)
+plane.position.y = -1.75
+plane.receiveShadow = true;
+scene.add(plane)
+
+const torusGeometry = [
+    new THREE.TorusGeometry(),
+    new THREE.TorusGeometry(),
+    new THREE.TorusGeometry(),
+    new THREE.TorusGeometry(),
+    new THREE.TorusGeometry(),
+]
+
+const material = [
+    new THREE.MeshBasicMaterial(),
+    new THREE.MeshLambertMaterial(),
+    new THREE.MeshPhongMaterial(),
+    new THREE.MeshPhysicalMaterial({}),
+    new THREE.MeshToonMaterial(),
+]
+
+const torus = [
+    new THREE.Mesh(torusGeometry[0], material[0]),
+    new THREE.Mesh(torusGeometry[1], material[1]),
+    new THREE.Mesh(torusGeometry[2], material[2]),
+    new THREE.Mesh(torusGeometry[3], material[3]),
+    new THREE.Mesh(torusGeometry[4], material[4]),
+]
+
+const texture = new THREE.TextureLoader().load('img/grid.png')
+material[0].map = texture
+material[1].map = texture
+material[2].map = texture
+material[3].map = texture
+material[4].map = texture
+
+torus[0].position.x = -8
+torus[1].position.x = -4
+torus[2].position.x = 0
+torus[3].position.x = 4
+torus[4].position.x = 8
+
+torus[0].castShadow = true
+torus[1].castShadow = true
+torus[2].castShadow = true
+torus[3].castShadow = true
+torus[4].castShadow = true
+
+torus[0].receiveShadow = true
+torus[1].receiveShadow = true
+torus[2].receiveShadow = true
+torus[3].receiveShadow = true
+torus[4].receiveShadow = true
+
+scene.add(torus[0])
+scene.add(torus[1])
+scene.add(torus[2])
+scene.add(torus[3])
+scene.add(torus[4])
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
 
-// resize event listener
-const onWindowResize = () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    render();
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const data = {
+    color: light.color.getHex(),
+    mapsEnabled: true,
+    //shadowMapSizeWidth: 512,
+    //shadowMapSizeHeight: 512,
 }
-window.addEventListener('resize', onWindowResize, false);
 
+const gui = new GUI()
+const lightFolder = gui.addFolder('THREE.Light')
+lightFolder.addColor(data, 'color').onChange(() => {
+    light.color.setHex(Number(data.color.toString().replace('#', '0x')))
+})
+lightFolder.add(light, 'intensity', 0, 1, 0.01)
 
-// Geometry
+const spotLightFolder = gui.addFolder('THREE.SpotLight')
+spotLightFolder.add(light, 'distance', 0, 100, 0.01)
+spotLightFolder.add(light, 'decay', 0, 4, 0.1)
+spotLightFolder.add(light, 'angle', 0, 1, 0.1)
+spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
+//spotLightFolder.add(light.shadow.camera, "near", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
+//spotLightFolder.add(light.shadow.camera, "far", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
+//spotLightFolder.add(data, "shadowMapSizeWidth", [256, 512, 1024, 2048, 4096]).onChange(() => updateShadowMapSize())
+//spotLightFolder.add(data, "shadowMapSizeHeight", [256, 512, 1024, 2048, 4096]).onChange(() => updateShadowMapSize())
+spotLightFolder.add(light.position, 'x', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'y', -50, 50, 0.01)
+spotLightFolder.add(light.position, 'z', -50, 50, 0.01)
+spotLightFolder.open()
 
-// Plane
-const planeGeom = new THREE.PlaneGeometry(100, 100, 50, 50);
-const planeMaterial = new THREE.MeshBasicMaterial({wireframe: true});
-const plane = new THREE.Mesh(planeGeom, planeMaterial);
-plane.rotateX(Math.PI / 2);
-scene.add(plane);
+// function updateShadowMapSize() {
+//     light.shadow.mapSize.width = data.shadowMapSizeWidth
+//     light.shadow.mapSize.height = data.shadowMapSizeHeight
+//     ;(light.shadow.map as any) = null
+// }
 
-// Cube
-const cubeGeom = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshNormalMaterial({transparent: true});
-const cube = new THREE.Mesh(cubeGeom, cubeMaterial);
-cube.position.y = 1;
-scene.add(cube);
-
-// controls
-const orbitControls = new OrbitControls(camera, renderer.domElement);
-orbitControls.minAzimuthAngle = 0;
-orbitControls.maxAzimuthAngle = Math.PI / 4;
-orbitControls.minPolarAngle = Math.PI / 4;
-orbitControls.maxPolarAngle = Math.PI / 2
-const dragControls = new DragControls([cube], camera, renderer.domElement);
-
-dragControls.addEventListener('dragstart', (event) => {
-    orbitControls.enabled = false;
-    event.object.material.opacity = 0.5;
+const meshesFolder = gui.addFolder('Meshes')
+meshesFolder.add(data, 'mapsEnabled').onChange(() => {
+    material.forEach((m) => {
+        if (data.mapsEnabled) {
+            m.map = texture
+        } else {
+            m.map = null
+        }
+        m.needsUpdate = true
+    })
 })
 
-dragControls.addEventListener('dragend', (event) => {
-    orbitControls.enabled = true;
-    event.object.material.opacity = 1;
-})
+function animate() {
+    requestAnimationFrame(animate)
 
-// stats
-const stats = Stats();
-document.body.appendChild(stats.dom);
+    helper.update()
 
-// animation
-const animate = () => {
-    requestAnimationFrame( animate );
-    render();
-    stats.update();
+    torus.forEach((t) => {
+        t.rotation.y += 0.01
+    })
+
+    render()
+
+    stats.update()
 }
 
-animate();
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
